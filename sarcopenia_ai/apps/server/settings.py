8ec01e1693626a83f93bc5720c0ad2ef9671d873
Kayla Janos <kaylajanos@Kaylas-MacBook-Pro.local>
2021-04-27 22:50:55 -0400

    Uploading remaining framework

---
+++ sarcopenia_ai/apps/server/settings.py
@@ -0,0 +1,11 @@
+UPLOAD_FOLDER = '/tmp'
+ALLOWED_EXTENSIONS = set(['.nii', '.nii.gz'])
+
+#SLICE_DETECTION_MODEL_PATH = '/sarcopenia_ai/models/slice_detection/'
+#SEGMENTATION_MODEL_PATH = '/sarcopenia_ai/models/slice_segmentation/'
+
+SLICE_DETECTION_MODEL_PATH = 'sarcopenia_ai/models/slice_detection/'
+SEGMENTATION_MODEL_PATH = 'sarcopenia_ai/models/slice_segmentation/'
+
+THRESHOLD_U=0.12
+THRESHOLD_L=0.00
